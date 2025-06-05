f6156c85870eb75339f5ea875997aa2b9d63ac9c
EdisonZheng2323 <edisonzheng3737@gmail.com>
2025-06-05 19:56:59 -0400

    Change GetKeywordProductFromAllCompanies

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -1,20 +1,9 @@
 import * as db from '../db/queries.js'
 
-export async function getKeyWordProductFromAllCompanies(req, res) { //Returns an array with each indices containing an array that contains all of the products with the same name. For example, [[switch (eBay), switch (bestBuy)], [sweets (eBay), sweets (bestBuy)]]
+export async function getKeyWordProductInSearch(req, res) {
     try {
         const dataRows = await db.getAllProductsThatStartWithKeyword(req.params.productName.toLowerCase());
-        let current = null;
-        const allProductStartingWithKeyword = [];
-        let currentArr = -1;
-        for(let i = 0; i < dataRows.length; i++){
-            if(current !== dataRows[i].product_name){
-                current = dataRows[i].product_name;
-                currentArr++;
-                allProductStartingWithKeyword[currentArr] = [];
-            }
-            allProductStartingWithKeyword[currentArr].push(dataRows[i]);
-        }
-        res.json(allProductStartingWithKeyword); 
+        res.json(dataRows); 
     }
     catch(err){
         console.error(err)
